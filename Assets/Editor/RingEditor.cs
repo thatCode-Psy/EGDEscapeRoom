831c212e21d51bcddbbf8a4172a200197d35a142
Jared <okunj20@gmail.com>
2020-02-17 22:33:46 -0500

    ring game is ready for testing

---
+++ Assets/Editor/RingEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RingScript))]
+public class RingEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        RingScript script = target as RingScript;
+        DrawDefaultInspector();
+
+        for(int i = 0; i < script.rings.Length; ++i)
+        {
+            if(i < script.currentAngle.Length)
+            {
+                script.rings[i].transform.localEulerAngles = new Vector3(0, script.currentAngle[i], 0);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/EGDScripts/RingScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingScript : TriggeringObject
+{
+    public string rotatingObjectTag;
+
+    public GameObject[] rings;
+
+    public float[] currentAngle;
+
+    public float tolerance;
+
+    public float transitionTimeForTolerance;
+
+    GameObject rotatingObject;
+
+    int ringNum;
+
+    float startTimer;
+
+    float currentDefaultAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotatingObject = GameObject.FindGameObjectWithTag(rotatingObjectTag);
+        ringNum = 0;
+        startTimer = 0f;
+        currentDefaultAngle = rotatingObject.transform.localEulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsTriggered())
+        {
+            float angleCheck = rotatingObject.transform.localEulerAngles.y + currentDefaultAngle;
+            currentAngle[ringNum] = angleCheck;
+            if (AngleWithinMargin(angleCheck))
+            {
+                if(startTimer < 0.00001f)
+                {
+                    startTimer = Time.time;
+                }
+                else if(Time.time - startTimer >= transitionTimeForTolerance)
+                {
+                    startTimer = 0f;
+                    ++ringNum;
+                    currentDefaultAngle = rotatingObject.transform.localEulerAngles.y;
+                }
+            }
+        }
+    }
+
+    bool AngleWithinMargin(float angle)
+    {
+        while(angle > 180f)
+        {
+            angle -= 360f;
+        }
+        while(angle <= -180f)
+        {
+            angle += 360f;
+        }
+        return Mathf.Abs(angle) < tolerance;
+    }
+
+    public override bool IsTriggered()
+    {
+        return ringNum >= rings.Length;
+    }
+}
